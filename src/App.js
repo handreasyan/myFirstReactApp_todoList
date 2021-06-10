a84efc491ba3a4b24ac82d6d6040b44daff960de
handreasyan <handreasyan98@gmail.com>
2021-06-11 00:53:12 +0400

    used useReducer

--- src/App.js
+++ src/App.js
@@ -2,39 +2,76 @@ import "./App.css";
 import { Form } from "./form";
 import { List } from "./list";
 import { Footer } from "./footer";
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  if (action.type === "delete_all") {
+    return state.filter((el) => !el.isCompleted);
+  } else if (action.type === "changeChecked") {
+    return state.map((el) => {
+      return el.id === action.payload.obj.id ? action.payload.obj : el;
+    });
+  } else if (action.type === "add") {
+    return [
+      ...state,
+      {
+        id: state.length + 1,
+        isCompleted: false,
+        text: action.payload.text,
+      },
+    ];
+  } else if (action.type === "delete") {
+    return state.filter((el) => el.id !== action.payload.id);
+  }
+}
 
 function App() {
-  let [data, setData] = useState([
+  const [data, dispatch] = useReducer(reducer, [
     { id: 1, isCompleted: true, text: "Learn HTML" },
     { id: 2, isCompleted: true, text: "Learn CSS" },
     { id: 3, isCompleted: true, text: "Learn JS" },
     { id: 4, isCompleted: false, text: "Learn React.js" },
     { id: 5, isCompleted: false, text: "Learn Node.js" },
   ]);
-  function addTodo(text) {
-    setData([
-      ...data,
-      {
-        id: data.length + 1,
-        isCompleted: false,
-        text,
-      },
-    ]);
-  }
-  function changeCheckbox(obj) {
-    setData(data.map((el) => (el.id === obj.id ? obj : el)));
-  }
-  function onDelete(id) {
-    setData(data.filter((el) => el.id !== id));
-  }
+
   return (
     <div className="App">
-      <Form addFunc={addTodo} />
-      <List data={data} changeCheckbox={changeCheckbox} onDelete={onDelete} />
+      <Form
+        addFunc={(text) => {
+          dispatch({
+            type: "add",
+            payload: {
+              text,
+            },
+          });
+        }}
+      />
+      <List
+        data={data}
+        changeCheckbox={(obj) => {
+          dispatch({
+            type: "changeChecked",
+            payload: {
+              obj: obj,
+            },
+          });
+        }}
+        onDelete={(id) => {
+          dispatch({
+            type: "delete",
+            payload: {
+              id,
+            },
+          });
+        }}
+      />
       <Footer
         data={data}
-        deleteAll={() => setData(data.filter((el) => !el.isCompleted))}
+        deleteAll={() => {
+          dispatch({
+            type: "delete_all",
+          });
+        }}
       />
     </div>
   );
